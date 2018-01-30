6489ece6d3157b31046006b21777d263f50b81a8
Javier Almoros Fernandez <javieraf@gamma02.aulas.gsyc.urjc.es>
2018-01-30 10:49:46 +0100

    Versión final de las tablas de multiplicar

---
+++ multiplica.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python3
+
+for numero1 in range(1,11):
+    print("Tabla de multiplicar del", numero1)
+    for numero2 in range(1,11):
+        print(numero1 , "por" , numero2, "es", numero1*numero2)
